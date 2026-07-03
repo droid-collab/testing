88288074a28ba646ac47ae10723c15c7f94fa77d
Mikhail Dsouza <mikhaildsouza2509@gmail.com>
2026-07-03 17:42:27 +0530

    Restore WISMUN logo image with empty alt text

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,6 +21,7 @@ export default function Navbar() {
         {/* Logo */}
         <Link to="/" className="navbar__logo" onClick={closeMenu}>
           <img src="https://wismun.in/img/logo-brand/gsg.png"    alt="GSG"     className="navbar__logo-img" />
+          <img src="https://wismun.in/img/logo-brand/wismun.png" alt=""  className="navbar__logo-img navbar__logo-img--wismun" />
           <img src="https://wismun.in/img/logo-brand/witty.png"  alt="Witty International School" className="navbar__logo-img" />
           <span className="navbar__logo-text">
             <span className="navbar__logo-main">WISMUN</span>
